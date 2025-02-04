f9302fe5e8e2578c51b120558b69e61b3cffc4b6
FernandoPM28 <ferpivamendes@gmail.com>
2025-02-04 16:31:05 -0300

    atualizado código para enviar

---
+++ ESP32_Code/PrintData/PrintData.ino
@@ -0,0 +1,15 @@
+#include <HardwareSerial.h>
+// Use uma porta serial secundária (ex.: Serial1)
+HardwareSerial mySerial(1);
+void setup() {
+  Serial.begin(9600);          // Monitor Serial do ESP32
+  mySerial.begin(9600, SERIAL_8N1, 16, 17); // RX=16, TX=17
+}
+void loop() {
+  if (mySerial.available()) {
+    // Lê os dados enviados pelo Arduino
+    Serial.print("Recebido do Arduino: ");
+    String sensorData = mySerial.readStringUntil('\n');
+    Serial.println(sensorData);
+  }
+}
